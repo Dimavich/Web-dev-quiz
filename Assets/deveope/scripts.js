aa3a680301f1338c7d6f795ff6ba006ac5aba1d0
Dmitriy <dmitriy.babich0@gmail.com>
2021-04-14 16:51:55 -0700

    made the html and css also added the variables in javaScript

---
+++ Assets/deveope/scripts.js
@@ -0,0 +1,12 @@
+// variables 
+var startBtn = document.getElementsByClassName('title');
+var quiz =  document.getElementsByClassName('quiz');
+var clock =  document.getElementsById('clock');
+var question =  document.getElementsById('question');
+var answers =  document.getElementsByClassName('ans-list');
+var a =  document.getElementsById('a');
+var b =  document.getElementsById('b');
+var c =  document.getElementsById('c');
+var d =  document.getElementsById('d');
+
+
